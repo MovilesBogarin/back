5ff4145b5f3ab39636baf5f62e5cdd323bc9a9bd
Daniel Rodriguez <daniel.rodriguez@hermosillo.com>
2023-11-27 20:50:32 -0800

    changes

--- src/controllers/recipe.controller.js
+++ src/controllers/recipe.controller.js
@@ -2,16 +2,25 @@ const asyncHandler = require('express-async-handler');
 const {recipes} = require('../static/static');
 
 exports.getRecipes = asyncHandler(async (req, res) => {
+    const formattedRecipes = recipes.reduce((recipe) => {
+        return {
+            id: recipe.id,
+            name: recipe.name,
+            description: recipe.description,
+        };
+    });
     console.log('recetas consultadas');
-    res.status(200).send(recipes);
+    res.status(200).send(formattedRecipes);
 });
 
 exports.getRecipe = asyncHandler(async (req, res) => {
     const recipeId = parseInt(req.params.id);
     const recipe = recipes.find(recipe => recipeId === recipe.id);
     if (recipe) {
+        console.log('receta '+recipeId+' consultada');
         res.status(200).send(recipe);
     } else {
+        console.log('receta '+recipeId+' no encontrada');
         res.status(404).send('Recipe not found');
     }
 });
--- src/static/static.js
+++ src/static/static.js
@@ -1,12 +1,36 @@
 exports.recipes = [
     {
-        'id': 1,
-        'name': 'Pasta',
-        'description': 'Pasta with tomato sauce',
-    },
-    {
-        'id': 2,
-        'name': 'Pizza',
-        'description': 'Pizza with tomato sauce',
+      'id': 1,
+      'name': 'Pasta',
+      'description': 'Pasta with tomato sauce',
+      'ingredients': [
+        {'name': 'Pasta', 'quantity': 200, 'unit': 'gr'},
+        {'name': 'Tomato', 'quantity': 2, 'unit': 'u'},
+        {'name': 'Salt', 'quantity': 1, 'unit': 'pz'},
+        {'name': 'Pepper', 'quantity': 1, 'unit': 'pz'},
+      ],
+      'steps': [
+        'Boil the water',
+        'Add the pasta',
+        'Cook it',
+        'Add the tomato sauce',
+        'Add the salt and pepper',
+      ]
+    }, {
+      'id': 2,
+      'name': 'Pizza',
+      'description': 'Pizza with tomato sauce',
+      'ingredients': [
+        {'name': 'Flour', 'quantity': 200.5, 'unit': 'gr'},
+        {'name': 'Tomato', 'quantity': 2, 'unit': 'u'},
+        {'name': 'Salt', 'quantity': 1, 'unit': 'pz'},
+        {'name': 'Pepperoni', 'quantity': 60, 'unit': 'gr'},
+      ],
+      'steps': [
+        'Make the dough',
+        'Add the tomato sauce',
+        'Add the pepperoni',
+        'Bake it',
+      ]
     }
-];
+  ];
