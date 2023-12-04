c7f4e1fca0eb2d7edbb6fb1e3df013c544abaeae
Javier Garcia <garcia.luzanilla.javier@gmail.com>
2023-12-03 18:02:45 -0800

    ME ESTABADANDO ERROR PERDON

--- src/controllers/schedule_recipe.controller.js
+++ src/controllers/schedule_recipe.controller.js
@@ -1,16 +1,17 @@
-const dayjs = require('dayjs');
 const asyncHandler = require('express-async-handler');
-const {schedule_recipes, recipes} = require('../static/static');
+const {schedule_recipes} = require('../static/static');
 
 exports.getScheduledRecipes = asyncHandler(async (req, res) => {
     console.log('Recetas agendadas .|.');
     res.status(200).send(schedule_recipes);
 });
 
+
+
 exports.createScheduledRecipe = asyncHandler(async (req, res) => {
     try{
-        const {id_schedule,id_recipe, quantity, date, recipe} = req.body;
-        schedule_recipes.push({id_schedule,id_recipe, quantity, date,recipe});
+        const {id_schedule,id_recipe, quantity, date,recipe} = req.body;
+        schedule_recipes.push({id_schedule,id_recipe, quantity, date, recipe});
         console.log('receta '+id_recipe+' con fecha: '+date +'guardada exitosamente');
         res.status(200).send('OK');
     } catch (error) {
@@ -51,3 +52,47 @@ exports.deleteScheduleRecipe = asyncHandler(async (req, res) => {
     }
 });
 
+
+
+/*
+exports.getRecipes = asyncHandler(async (req, res) => {
+    console.log('recetas consultadas');
+    res.status(200).send(recipes);
+});
+
+exports.createRecipe = asyncHandler(async (req, res) => {
+    try{
+        const {id, name, description, ingredients, steps} = req.body;
+        recipes.push({id, name, description, ingredients, steps});
+        console.log('receta '+id+' creada correctamente');
+        res.status(200).send('OK');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+exports.updateRecipe = asyncHandler(async (req, res) => {
+    try {
+        const {id, name, description, ingredients, steps} = req.body;
+        const index = recipes.findIndex(recipe => recipe.id === id);
+        recipes[index] = {id, name, description, ingredients, steps};
+        console.log('receta '+id+' actualizada correctamente');
+        res.status(200).send('OK');
+
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+exports.deleteRecipe = asyncHandler(async (req, res) => {
+    try {
+        const {id} = req.params;
+        const index = recipes.findIndex(recipe => recipe.id === parseInt(id));
+        recipes.splice(index, 1);
+        console.log('receta '+id+' eliminada correctamente');
+        res.status(200).send('OK');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+*/
